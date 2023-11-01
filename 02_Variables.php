960107c5c2b95135604bb0f02335ae4fbd06ecd2
zhiqian <yapzq-wm22@student.tarc.edu.my>
2023-11-01 22:57:21 +0800

    Create 02_Variables.php

---
+++ 02_Variables.php
@@ -0,0 +1,17 @@
+<?php
+
+    #boolean
+    $logged_in = true;      //default = false
+
+    #integer
+    $num = 2940;
+
+    #floating point number
+    $speed = 104.87;
+
+    #String - use " " or ' '
+    $name = "John";
+
+    echo "My name is $name !";
+
+?>
